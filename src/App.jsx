42ae7cf4bbe30037b7d7b37bd13dc8a73e7bf94b
agwadan <agwa.dan@gmail.com>
2022-01-11 16:49:07 +0300

    Create contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Contact from './components/contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Agwa</h1>
       </header>
+      <Contact />
     </div>
   )
 }
