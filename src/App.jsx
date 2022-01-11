c729a191a812699d040ae134154945da048645ba
agwadan <agwa.dan@gmail.com>
2022-01-11 21:49:44 +0300

    Initialize landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Contact from './components/contact'
+import LandingPage from './components/landingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Agwa</h1>
-      </header>
-      <Contact />
+      <LandingPage />
     </div>
   )
 }
---
+++ src/components/landingPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Contact from '../contact';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Agwa</h1>
+      </header>
+      <Contact />
+    </div>
+  )
+}
+
+export default LandingPage;
