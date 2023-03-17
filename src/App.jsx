b33cc081919bc4dbb7082c8a0c552d5478496ba3
agwadan <agwa.dan@gmail.com>
2023-03-17 10:23:18 +0300

    Add CSS Variables and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import LandingPage from './components/landingPage';
+import Header from './components/header';
+import Nav from './components/nav';
+import Experience from './components/experience';
+import About from './components/about';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <LandingPage />
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
     </div>
   )
 }
