9e56288ab4c401f3e324b11623a0b9bfd22502fa
agwadan <agwa.dan@gmail.com>
2023-03-17 12:16:14 +0300

    Add Name and image to header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import CTA from '../CTA';
+import ProfileImg from '../../../assets/images/profile.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Daniel Agwa</h1>
+        <h5>Frontend Developer</h5>
+        <CTA />
+        <div className="profile-img">
+          <img src={ProfileImg} alt="" />
+        </div>
+      </div>
+    </header>
   )
 }
 
