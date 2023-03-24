b7b2563b2829408e76a5067a515809770eb09e4a
agwadan <agwa.dan@gmail.com>
2023-03-24 11:08:50 +0300

    Complete working on about component

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import './about.css';
+import ProfileImg from '../../../assets/images/profile.png';
+import { FaAward } from 'react-icons/fa';
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h1>Get to Know</h1>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about_me_image">
+            <img src={ProfileImg} alt="Profile Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          {/*   <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years of Frontend Development</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years of Frontend Development</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years of Frontend Development</small>
+            </article>
+          </div> */}
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore doloremque nulla nihil, quaerat illum fugiat nostrum repellendus numquam. Reiciendis deserunt officiis quibusdam nam, sed tempore quas odit asperiores iure dignissimos incidunt dolorem ratione libero a aut in! Recusandae, pariatur nam eos nihil facilis harum reprehenderit?</p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
