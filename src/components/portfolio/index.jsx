0237140b13473e2d9d2b8bbdfd49e4772ff02c89
agwadan <agwa.dan@gmail.com>
2023-03-25 16:50:07 +0300

    Fix minor bugs

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -19,8 +19,8 @@ const Portfolio = () => {
                 </div>
                 <h3>{title}</h3>
                 <div className="portfolio__item_cta">
-                  <a href={github} className='btn'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Agwa</a>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
                 </div>
               </article>
             )
