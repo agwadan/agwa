9dd467952dfa569328aca71fb429eba063b8fb9a
agwadan <agwa.dan@gmail.com>
2023-05-13 19:11:46 +0300

    Update hero sectio

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,21 +8,27 @@ import { AiOutlineArrowDown } from 'react-icons/ai';
 const Header = () => {
   return (
     <header>
-      <div className="container header__container">
-        <h5>Hello I am</h5>
-        <h1>Daniel Agwa</h1>
-        <h5>Frontend Developer</h5>
-        <CTA />{/* Call to action */}
-        <HeaderSocials />
-        <div className="profile__img">
-          <img src={ProfileImg} alt="" />
+      <div className="container header__container grid__2">
+        <div className="left">
+
+          <h5>Hello I am</h5>
+          <h1>Daniel Agwa</h1>
+          <h5>Frontend Developer</h5>
+          <CTA />{/* Call to action */}
         </div>
 
-        <a href="#contact" className="scroll__down">
-          <AiOutlineArrowDown />
-        </a>
+        <div className="right">
+          <HeaderSocials />
+          <div className="profile__img">
+            <img src={ProfileImg} alt="" />
+          </div>
+
+          <a href="#contact" className="scroll__down">
+            <AiOutlineArrowDown />
+          </a>
+        </div>
       </div>
-    </header>
+    </header >
   )
 }
 
