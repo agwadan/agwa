5abc3a628decd63060192514eadaaaf8ec11365c
agwadan <agwa.dan@gmail.com>
2023-08-10 15:36:49 +0300

    Add countries app to portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,27 +1,34 @@
 const data = [
   {
     id: 1,
+    image: '/images/portfolio_countries_app.png',
+    title: 'Where in the World?',
+    github: 'https://github.com/agwadan/Countries-App',
+    demo: 'https://agwadan.github.io/Countries-App/'
+  },
+  {
+    id: 2,
     image: '/images/portfolio_1.png',
     title: 'Where Cinema Lovers Unite',
     github: 'https://github.com/agwadan/nuxtjs-movie-app',
     demo: 'https://nuxtmovieapp.netlify.app/'
   },
   {
-    id: 2,
+    id: 3,
     image: '/images/portfolio_2.png',
     title: 'Fast, Secure Hosting Demo',
     github: 'https://github.com/agwadan/hosting-website',
     demo: 'https://loruki-clone1.netlify.app/'
   },
   {
-    id: 3,
+    id: 4,
     image: '/images/portfolio_3.png',
     title: 'React 3D Demo',
     github: 'https://github.com/agwadan/react-3d',
     demo: 'https://react-3d-demo.netlify.app/'
   },
   {
-    id: 1,
+    id: 5,
     image: '/images/portfolio_4.png',
     title: 'Aya Landing Page v1',
     github: 'https://github.com/agwadan/',
