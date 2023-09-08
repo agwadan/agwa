9cfbb44e3c8b359899aa21953c4cace59744dfc2
agwadan <agwa.dan@gmail.com>
2023-09-08 14:34:40 +0300

    Add react router and transcribe page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
-import Header from './components/header';
-import Nav from './components/nav';
-import Experience from './components/experience';
-import About from './components/about';
-import Contact from './components/contact';
-import Portfolio from './components/portfolio';
-import Footer from './components/footer';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Nav from "./components/nav";
+import Experience from "./components/experience";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Portfolio from "./components/portfolio";
+import Footer from "./components/footer";
+import MyTranscriptionPage from "./components/transcription";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPortfolio />} />
+        <Route path="/transcribe" element={<MyTranscriptionPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+export const MainPortfolio = () => {
   return (
     <div className="App">
       <Header />
@@ -21,7 +35,5 @@ function App() {
       <Contact />
       <Footer />
     </div>
-  )
-}
-
-export default App
+  );
+};
