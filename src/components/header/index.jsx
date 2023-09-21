6be7d9b962916661179b285b5d7392d2e74a88ac
agwadan <agwa.dan@gmail.com>
2023-09-21 10:50:49 +0300

    Update Landing page view and social links

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import './header.css';
-import CTA from './CTA';
-import ProfileImg from '/images/profile.png'
-import HeaderSocials from './HeaderSocials';
-import { AiOutlineArrowDown } from 'react-icons/ai';
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ProfileImg from "/images/profile.png";
+import HeaderSocials from "./HeaderSocials";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container grid__2">
         <div className="left">
-
-          <img className='smallscreen_img' src={ProfileImg} alt="" />
+          <img className="smallscreen_img" src={ProfileImg} alt="" />
           <h5>Hello I am</h5>
           <h1>Daniel Agwa</h1>
           <h5>Frontend Developer</h5>
-          <CTA />{/* Call to action */}
-          
+          <CTA />
+          {/* Call to action */}
+          <HeaderSocials />
         </div>
 
         <div className="right">
-          <HeaderSocials />
           <div className="profile__img">
             <img src={ProfileImg} alt="" />
           </div>
@@ -30,8 +29,8 @@ const Header = () => {
           </a>
         </div>
       </div>
-    </header >
-  )
-}
+    </header>
+  );
+};
 
 export default Header;
