6a202e1f20e597d84c3f97ab59db7bb588ea0f05
agwadan <agwa.dan@gmail.com>
2023-12-21 22:36:48 +0300

    Update profile images

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './about.css';
-import ProfileImg from '/images/profile.png';
-import { FaAward } from 'react-icons/fa';
+import React from "react";
+import "./about.css";
+import ProfileImg from "/images/profile_3.jpg";
+import { FaAward } from "react-icons/fa";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
@@ -17,12 +17,23 @@ const About = () => {
         </div>
 
         <div className="about__content">
-          <p>Hello, my name is Daniel Agwa and I am a frontend software developer. With several years of experience in the field, I have honed my skills in HTML, CSS, JavaScript, and other key frontend technologies. I am passionate about building beautiful, responsive, and user-friendly interfaces that improve the user experience. As a dedicated learner, I am always exploring new technologies and techniques to stay ahead of the curve. When I'm not coding, I enjoy spending time outdoors, making music, and traveling.</p>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <p>
+            Hello, my name is Daniel Agwa and I am a frontend software
+            developer. With several years of experience in the field, I have
+            honed my skills in HTML, CSS, JavaScript, and other key frontend
+            technologies. I am passionate about building beautiful, responsive,
+            and user-friendly interfaces that improve the user experience. As a
+            dedicated learner, I am always exploring new technologies and
+            techniques to stay ahead of the curve. When I'm not coding, I enjoy
+            spending time outdoors, making music, and traveling.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ProfileImg from "/images/profile.png";
+import ProfileImg from "/images/profile_2.jpg";
 import HeaderSocials from "./HeaderSocials";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { BsChevronDoubleDown } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 const Header = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
   return (
-    <header>
+    <header ref={ref}>
       <div className="container header__container grid__2">
         <div className="left">
           <img className="smallscreen_img" src={ProfileImg} alt="" />
@@ -25,7 +27,7 @@ const Header = () => {
           <motion.div
             className="profile__img"
             initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
+            animate={isInView ? { opacity: 1, x: 0 } : {}}
             transition={{ duration: 1 }}
           >
             <img src={ProfileImg} alt="" />
