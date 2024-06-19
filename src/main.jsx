614567e3a8906257ddd70ac32e082256bb6074eb
agwadan <agwa.dan@gmail.com>
2024-06-19 10:33:20 +0300

    Install google analytics and measurement ID

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-PMQMHEHK2H");
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
