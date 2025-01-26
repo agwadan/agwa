db25de38f1b48925e22da808c82a6db9748b4882
agwadan <agwa.dan@gmail.com>
2025-01-26 23:13:57 +0300

    Update link for movie scout

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,44 +1,44 @@
 const data = [
   {
     id: 2,
-    image: '/images/portfolio_1.png',
-    title: 'Where Cinema Lovers Unite',
-    github: 'https://github.com/agwadan/nuxtjs-movie-app',
-    demo: 'https://nuxtmovieapp.netlify.app/'
+    image: "/images/portfolio_1.png",
+    title: "Where Cinema Lovers Unite",
+    github: "https://github.com/agwadan/nuxtjs-movie-app",
+    demo: "https://moviescout22.netlify.app/",
   },
   {
     id: 1,
-    image: '/images/portfolio_countries_app.png',
-    title: 'Where in the World?',
-    github: 'https://github.com/agwadan/Countries-App',
-    demo: 'https://agwadan.github.io/Countries-App/'
+    image: "/images/portfolio_countries_app.png",
+    title: "Where in the World?",
+    github: "https://github.com/agwadan/Countries-App",
+    demo: "https://agwadan.github.io/Countries-App/",
   },
   {
     id: 7,
-    image: '/images/portfolio_ello.png',
-    title: 'Aid Learning Through Reading',
-    github: 'https://github.com/agwadan/ello-assignment',
+    image: "/images/portfolio_ello.png",
+    title: "Aid Learning Through Reading",
+    github: "https://github.com/agwadan/ello-assignment",
   },
   {
     id: 3,
-    image: '/images/portfolio_weather_app.png',
-    title: 'Weather App',
-    github: 'https://github.com/agwadan/Weather-App',
-    demo: 'https://agwadan.github.io/Weather-App/'
+    image: "/images/portfolio_weather_app.png",
+    title: "Weather App",
+    github: "https://github.com/agwadan/Weather-App",
+    demo: "https://agwadan.github.io/Weather-App/",
   },
   {
     id: 4,
-    image: '/images/portfolio_2.png',
-    title: 'Fast, Secure Hosting Demo',
-    github: 'https://github.com/agwadan/hosting-website',
-    demo: 'https://loruki-clone1.netlify.app/'
+    image: "/images/portfolio_2.png",
+    title: "Fast, Secure Hosting Demo",
+    github: "https://github.com/agwadan/hosting-website",
+    demo: "https://loruki-clone1.netlify.app/",
   },
   {
     id: 5,
-    image: '/images/portfolio_job_postings.png',
-    title: 'Hatch Your Next Job',
-    github: 'https://github.com/agwadan/lara-gigs',
+    image: "/images/portfolio_job_postings.png",
+    title: "Hatch Your Next Job",
+    github: "https://github.com/agwadan/lara-gigs",
   },
-]
+];
 
 export default data;
